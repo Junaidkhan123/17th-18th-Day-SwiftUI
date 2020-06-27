6cca26986b77175e3b1b6a8a00ee36a25649a368
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-06-27 13:35:35 +0500

    Completd 17th and 18th Day of SwiftUI with small challenge

--- 17th-Day-SwiftUI/ContentView.swift
+++ 17th-Day-SwiftUI/ContentView.swift
@@ -9,8 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 2.0
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var originalPlusTipValue: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        
+        return grandTotal
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Number of People", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section(header: Text("How much tip do you want to Leeave?")){
+                    
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0]) %")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("Amount per person")){
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section (header: Text("total Amount for check")){
+                    Text("$\(originalPlusTipValue, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("We Split")
+        }
     }
 }
 
